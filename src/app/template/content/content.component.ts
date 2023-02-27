ecb321059731af9b7b284bc6a980e28061be0bb6
shcodervik <shcodervik@gmail.com>
2023-02-27 09:53:22 +0300

    Splitter finished.

--- src/app/template/content/content.component.ts
+++ src/app/template/content/content.component.ts
@@ -1,7 +1,5 @@
-import { ElementRef, AfterViewInit } from '@angular/core';
-import { ViewChild } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
-import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-content',
@@ -9,41 +7,30 @@ import { fromEvent } from 'rxjs';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements AfterViewInit {
-  //@ViewChild('resizer') resizer!: ElementRef;
-
 
   constructor() {}
 
-  ngAfterViewInit() {
+  ngAfterViewInit() {  }
 
-
-
-  }
   mousedown(event: any) {
     event.preventDefault();
     document.getElementById('panel')!.addEventListener('pointermove', this.handleSplitterMove);
-
-    console.log("I'm here!")
   }
 
   handleSplitterMove(event: any) {
-    console.log(event.x + " " + event.y);
     const panel = document.getElementById('panel')!;
     const box1 = document.getElementById('sidebar')!;
     const box2 = document.getElementById('main')!;
     const splitter = document.getElementById('dragbar')!.getBoundingClientRect();
-    box1.style.width = event.x+"px";
-    const difference = Number(panel.style.width) - Number(box1.style.width);
-    box2.style.width = difference+"px";
+    box1.style.width = event.x + "px";
+    const difference = panel.getBoundingClientRect().width - splitter.width - event.x;
+    box2.style.width = difference + "px";
     box2.style.left = box1.style.width + "px";
-
-
   }
 
   mouseup(event: any) {
     event.preventDefault();
     document.getElementById('panel')!.removeEventListener('pointermove', this.handleSplitterMove);
-    console.log("And now here!");
   }
 
 }
